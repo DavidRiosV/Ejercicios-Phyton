f1a0ad5eff4d06db849d1267fa0a1a782a0e20a5
David Rios <david.rios.valencia.al@iespoligonosur.org>
2024-09-24 19:40:01 +0200

    Ejercicio7

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,5 +1,5 @@
 #Convierte grados Celsius a Fahrenheit
 print("Ingresa tus grados celsius")
 c = input()
-f= (float(c) * 9 / 5) + 32)
-print("En grados fahrenheit es" +str(f))
+f= ((float(c) * 9 / 5) + 32)
+print("En grados fahrenheit es " +str(f))
--- ejercicio7.py
+++ ejercicio7.py
@@ -1,1 +1,18 @@
 #Calcula el máximo común divisor (MCD) de dos números. 
+print("Dime el primer numero")
+n1 = input()
+print("Dime el segundo numero")
+n2 = input()
+
+d = 0
+if n1 >= n2:
+   d = n1
+else:
+   d = n2
+
+maxd = 0
+for d in range (int(d), 1, -1):
+   if int(n1) % d == 0 and int(n2) % d == 0:
+       if d > maxd:
+           maxd = d
+           print("Maximo Comun Divisor es " +str(d))
